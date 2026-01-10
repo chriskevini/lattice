c34bcba458396a76f1b09e26e6d4176539435472
Chris Irineo <chriskevini@gmail.com>
2026-01-10 01:22:51 -0800

    Add exports for placeholder registry and injector

--- lattice/utils/__init__.py
+++ lattice/utils/__init__.py
@@ -1,1 +1,15 @@
 """Utilities package for Lattice."""
+
+from lattice.utils.placeholder_injector import PlaceholderInjector
+from lattice.utils.placeholder_registry import (
+    PlaceholderDef,
+    PlaceholderRegistry,
+    get_registry,
+)
+
+__all__ = [
+    "PlaceholderInjector",
+    "PlaceholderDef",
+    "PlaceholderRegistry",
+    "get_registry",
+]
